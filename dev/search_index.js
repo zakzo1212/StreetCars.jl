dcd44b66e91b3fe380a6993cf44051a505263943
Documenter.jl <documenter@juliadocs.github.io>
2023-11-02 23:53:43 +0000

    build based on 24ef9a7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StreetCars","category":"page"},{"location":"#StreetCars","page":"Home","title":"StreetCars","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StreetCars.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StreetCars]","category":"page"}]
+}
