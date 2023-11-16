6611fcf06757eb17b38635002a5ffb57a36a3355
Zachary Zhang <zakzo1212@gmail.com>
2023-11-15 20:10:46 -0500

    fix dependency issues;

--- src/StreetCars.jl
+++ src/StreetCars.jl
@@ -5,6 +5,9 @@ Package designed to interact with the data of the 2014 Google Hash Code Project
 """
 module StreetCars
 
+using Artifacts: @artifact_str
+using Random: AbstractRNG, default_rng
+
 export directed_random_walk
 export upper_bound
 
