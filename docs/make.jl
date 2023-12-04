899dcedd0df8985f1359ac4ce6dfeac6a3184e79
Zachary Zhang <zakzo1212@gmail.com>
2023-12-04 00:32:38 -0500

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,11 @@
 using StreetCars
+using HashCode2014
 using Documenter
 
 DocMeta.setdocmeta!(StreetCars, :DocTestSetup, :(using StreetCars); recursive=true)
 
 makedocs(;
-    modules=[StreetCars],
+    modules=[StreetCars, HashCode2014],
     authors="Zachary Zhang <zakzhang@mit.edum> and Andrew Jenkins <awj@mit.edu>",
     repo="https://github.com/zakzo1212/StreetCars.jl/blob/{commit}{path}#{line}",
     sitename="StreetCars.jl",
